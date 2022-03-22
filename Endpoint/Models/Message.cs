bbc265e75c3910fe1083a5c4cbf4afb2bd3f61f5
szabados.krisztian@stud.uni-obuda.hu <szabados.krisztian@stud.uni-obuda.hu>
2022-03-22 12:32:28 +0100

    models

---
+++ Endpoint/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Endpoint.Models
+{
+    public class Message
+    {
+        public string Msg { get; set; }
+
+        public DateTime DTStamp { get; set; }
+        public string SenderName { get; set; }
+    }
+}
